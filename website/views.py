d4eeea84b106c710665fd6408ea0b62d78fb9b56
Diego Cano <cpucano@gmail.com>
2023-05-26 17:59:51 -0500

    historia

--- website/views.py
+++ website/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
     #check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,7 +21,7 @@ def home(request):
             messages.success(request, "Error loguese de nuevo, intente de nuevo")
             return redirect('home')
     else:
-        return render(request, 'home.html',{})
+        return render(request, 'home.html',{'records':records})
 
 def logout_user(request):
     logout(request)
@@ -41,7 +43,16 @@ def register_user(request):
     else:
         form = SignUpForm()
         return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
 
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #look Up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "you Must be Logged In To View That Page..")
+        return redirect('home')
 
 
 
