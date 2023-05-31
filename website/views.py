1f7564db4c0a1d9cfaf57866e6edfaac263fe6b1
Diego Cano <cpucano@gmail.com>
2023-05-31 17:43:54 -0500

    Final Proyecto

--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, AddRecordForm
 from .models import Record
 
 
@@ -52,6 +52,45 @@ def customer_record(request, pk):
         return render(request, 'record.html', {'customer_record':customer_record})
     else:
         messages.success(request, "you Must be Logged In To View That Page..")
+        return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Elemento a sido borrado con exito")
+        return redirect('home')
+    else:
+        messages.success(request, "Debes estar logueado para realizar esta accion")
+        return redirect('home')
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added......")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "Record Added......")
+        return redirect('home')
+
+
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Has Been Updated!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form':form})
+    else:
+        messages.success(request, "Record Updated......")
         return redirect('home')
 
 
